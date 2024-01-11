88a3aeb59d7afbd8332c84500845c82cfd770552
massimo <massipetra@hotmail.it>
2024-01-11 09:21:26 +0100

    app-basic-configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { withComponentInputBinding } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideStore({}),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideAnimations()
+],
 };
