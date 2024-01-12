a35ff334eca1cbe29f600d9622494a05205b9075
massimo <massipetra@hotmail.it>
2024-01-12 12:53:39 +0100

    feat: form mushroom component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { MushroomTablePageComponent } from './features/mycology/mushroom-table-page/mushroom-table-page.component';
+import { FormMushroomPageComponent } from './features/mycology/form-mushroom-page/form-mushroom-page.component';
 
 export const routes: Routes = [
     {path: 'mushrooms/page/:pagenumber', component: MushroomTablePageComponent},
-    {path: '', redirectTo: 'mushrooms/page/1', pathMatch: 'prefix'}
+    {path: 'mushroom/:id/page/:page', component: FormMushroomPageComponent},
+    {path: '', redirectTo: 'mushrooms/page/1', pathMatch: 'prefix'},
+    
 ];
---
+++ src/app/features/mycology/form-mushroom-page/form-mushroom-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-mushroom-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-mushroom-page.component.html',
+  styleUrl: './form-mushroom-page.component.scss'
+})
+export class FormMushroomPageComponent {
+
+}
