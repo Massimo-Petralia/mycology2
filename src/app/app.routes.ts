5eb031b04ac0bd78c466c2613a9b73df19951262
massimo <massipetra@hotmail.it>
2024-01-12 12:09:36 +0100

    feat: path: 'mushrooms/page/:pagenumber' added to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { MushroomTablePageComponent } from './features/mycology/mushroom-table-page/mushroom-table-page.component';
 
 export const routes: Routes = [
-    {path: 'mushrooms', component: MushroomTablePageComponent},
-    {path: '', redirectTo: 'mushrooms', pathMatch: 'prefix'}
+    {path: 'mushrooms/page/:pagenumber', component: MushroomTablePageComponent},
+    {path: '', redirectTo: 'mushrooms/page/1', pathMatch: 'prefix'}
 ];
