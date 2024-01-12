72c16830a94f7feadbfd1395e8805211e70ff31a
massimo <massipetra@hotmail.it>
2024-01-12 16:13:14 +0100

    feat: sub form taxonomy and morphology added

--- src/app/features/mycology/form-mushroom/form-mushroom.component.ts
+++ src/app/features/mycology/form-mushroom/form-mushroom.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { Mushroom } from '../models/mycology.models';
 import { MatExpansionModule } from '@angular/material/expansion'
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-form-mushroom',
   standalone: true,
-  imports: [ReactiveFormsModule, MatExpansionModule],
+  imports: [ReactiveFormsModule, MatExpansionModule, MatInputModule],
   templateUrl: './form-mushroom.component.html',
   styleUrl: './form-mushroom.component.scss',
 })
@@ -17,10 +18,12 @@ export class FormMushroomComponent {
   formMushroom = this.formbuilder.group({
     taxonomy: this.formbuilder.group({
       species: this.formbuilder.control<string>('', Validators.required),
-      AA: this.formbuilder.control<string>(''),
       gender: this.formbuilder.control<string>(''),
       family: this.formbuilder.control<string>(''),
+      order: this.formbuilder.control<string>(''),
       synonymous: this.formbuilder.control<string>(''),
+      AA: this.formbuilder.control<string>('')
+
     }),
     morphology: this.formbuilder.group({
       cap: this.formbuilder.control<string>(''),
