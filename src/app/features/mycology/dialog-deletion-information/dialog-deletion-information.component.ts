0832ff07d33905e43f0c00cfbbb37124c1a32b1c
massimo <massipetra@hotmail.it>
2024-02-28 14:33:02 +0100

    feat:(mushroom-table, dialogbox component): added dialogbox on delete selected with alternative text

--- src/app/features/mycology/dialog-deletion-information/dialog-deletion-information.component.ts
+++ src/app/features/mycology/dialog-deletion-information/dialog-deletion-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
@@ -11,11 +11,25 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './dialog-deletion-information.component.html',
   styleUrl: './dialog-deletion-information.component.scss',
 })
-export class DialogDeletionInformationComponent {
+export class DialogDeletionInformationComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public inputData: any,
     private dialogRef: MatDialogRef<DialogDeletionInformationComponent>
   ) {}
+
+  text: string = '';
+  ngOnInit(): void {
+    console.log('data: ', this.inputData);
+    if (this.inputData instanceof Array) {
+      this.text = `Do you want to <strong>delete</strong> the <strong>selected mushrooms</strong> ?<br>
+      This operation will also <strong>delete</strong> the
+      <strong>iconographic galleries</strong> if present.`;
+    } else if (typeof this.inputData.species === 'string') {
+      this.text = `  Do you want to <strong>delete</strong> the mushroom
+    <strong>${this.inputData.species}</strong> ?<br> This operation will also <strong>delete</strong> the
+    <strong>iconographic gallery</strong> if present.`;
+    }
+  }
 
   closeDialog(del?: string) {
     this.dialogRef.close(del);
